f8ff84a461f094a6b62e172352f6ca4aed44a3da
Youssef baddi <baddiyoussef85@gmail.com>
2021-06-08 15:30:16 +0100

    add calcultrice

--- Chapter06/SimplestKeypad/SimplestKeypad/SimplestKeypad/SimplestKeypadPage.cs
+++ Chapter06/SimplestKeypad/SimplestKeypad/SimplestKeypad/SimplestKeypadPage.cs
@@ -1,82 +1,87 @@
 using System;
 using Xamarin.Forms;
 
-namespace Ch_4_ex1
+namespace SimplestKeypad
 {
-    internal class SimplesKeyPadPage : ContentPage
+    public class SimplestKeypadPage : ContentPage
     {
         Label displayLabel;
-        Button backspacceButton;
+        Button backspaceButton;
 
-        public SimplesKeyPadPage()
+        public SimplestKeypadPage()
         {
+            // Create a vertical stack for the entire keypad.
             StackLayout mainStack = new StackLayout
             {
                 VerticalOptions = LayoutOptions.Center,
                 HorizontalOptions = LayoutOptions.Center
             };
 
+            // First row is the Label.
             displayLabel = new Label
             {
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
                 VerticalOptions = LayoutOptions.Center,
-                HorizontalOptions = LayoutOptions.End
+                HorizontalTextAlignment = TextAlignment.End
             };
-
             mainStack.Children.Add(displayLabel);
 
-            backspacceButton = new Button
+            // Second row is the backspace Button.
+            backspaceButton = new Button
             {
-                Text ="\u21E6",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-               IsEnabled = false
+                Text = "\u21E6",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                IsEnabled = false
             };
+            backspaceButton.Clicked += OnBackspaceButtonClicked;
+            mainStack.Children.Add(backspaceButton);
 
-            backspacceButton.Clicked += OnBackspacceButtonClicked;
-            mainStack.Children.Add(backspacceButton);
-
+            // Now do the 10 number keys.
             StackLayout rowStack = null;
 
-            for (int num = 1; num <=10; num++)
+            for (int num = 1; num <= 10; num++)
             {
-                if((num -1) % 3 == 0)
+                if ((num - 1) % 3 == 0)
                 {
                     rowStack = new StackLayout
                     {
                         Orientation = StackOrientation.Horizontal
                     };
-                    rowStack.Children.Add(rowStack);
+                    mainStack.Children.Add(rowStack);
                 }
 
                 Button digitButton = new Button
                 {
                     Text = (num % 10).ToString(),
-                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                    
+                    FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                    StyleId = (num % 10).ToString()
                 };
                 digitButton.Clicked += OnDigitButtonClicked;
 
+                // For the zero button, expand to fill horizontally.
                 if (num == 10)
                 {
                     digitButton.HorizontalOptions = LayoutOptions.FillAndExpand;
                 }
                 rowStack.Children.Add(digitButton);
-                
-
             }
 
             this.Content = mainStack;
-
         }
 
-        private void OnDigitButtonClicked(object sender, EventArgs e)
+        void OnDigitButtonClicked(object sender, EventArgs args)
         {
-            throw new NotImplementedException();
+            Button button = (Button)sender;
+            displayLabel.Text += (string)button.StyleId;
+            backspaceButton.IsEnabled = true;
         }
 
-        private void OnBackspacceButtonClicked(object sender, EventArgs e)
+        void OnBackspaceButtonClicked(object sender, EventArgs args)
         {
-            throw new NotImplementedException();
+            
+            string text = displayLabel.Text;
+            displayLabel.Text = text.Substring(0, text.Length - 1);
+            backspaceButton.IsEnabled = displayLabel.Text.Length > 0;
         }
     }
 }

28333b8e5e4716a6c6e3c9c48c90b17d74691d99
Youssef baddi <baddiyoussef85@gmail.com>
2021-06-09 12:02:26 +0100

    add chaptrer08

--- Chapter08/ColorViewList/ColorViewList/ColorViewList/ColorView.xaml.cs
+++ Chapter08/ColorViewList/ColorViewList/ColorViewList/ColorView.xaml.cs
@@ -6,7 +6,7 @@ namespace ColorViewList
     public partial class ColorView : ContentView
     {
         string colorName;
-    
+        ColorTypeConverter colorTypeConv = new ColorTypeConverter();
 
         public ColorView()
         {
@@ -22,7 +22,8 @@ namespace ColorViewList
                 colorNameLabel.Text = value;
 
                 // Get the actual Color and set the other views.
-                
+                Color color = (Color)colorTypeConv.ConvertFromInvariantString(colorName);
+                boxView.Color = color;
                 colorValueLabel.Text = String.Format("{0:X2}-{1:X2}-{2:X2}",
                                                      (int)(255 * color.R), 
                                                      (int)(255 * color.G), 
